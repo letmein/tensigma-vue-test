fceb336ebf0737c5cfe0261192565ef802cc302c
Yuriy Kharchenko <rm1945@yandex.ru>
2020-12-07 11:35:02 +0300

    Add bulma

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/desktop">Desktop</router-link>
-    </div>
+    <nav id="nav" class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <router-link
+            class="navbar-item"
+            exact-active-class="has-text-primary"
+            to="/">
+            Home
+          </router-link>
+          <router-link
+            class="navbar-item"
+            exact-active-class="has-text-primary"
+            to="/desktop">
+            Desktop
+          </router-link>
+        </div>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
