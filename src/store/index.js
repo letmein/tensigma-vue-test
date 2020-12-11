35f595318977ef990dd1c13cb703034ffb558dce
Yuriy Kharchenko <rm1945@yandex.ru>
2020-12-11 22:22:48 +0300

    Persist tiles

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import desktop from './desktop'
+import VuexPersistence from 'vuex-persist'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     desktop
-  }
+  },
+  plugins: [vuexLocal.plugin],
 })
